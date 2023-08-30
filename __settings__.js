415b783b3c8e6c61c4f472ecf57012dad6d18ff2
koval122 <48149074+koval122@users.noreply.github.com>
2023-08-30 13:21:00 +0400

    Add files via upload

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1844576.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 147491474, 147491475, 147491476, 147491477, 147491846 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/147491498/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/147491499/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/147491497/1/ammo.js', 'preload' : true},
+];
